8b9cdc8026984850b0fcb9964f835b230e3b8cb9
MosesSupposes <mosamuel98@gmail.com>
2019-02-04 15:17:09 -0800

    add checks handler to router

--- index.js
+++ index.js
@@ -93,5 +93,6 @@ function handleRequest(req, res) {
 const router = {
     ping: handlers.ping,
     users: handlers.users,
-    tokens: handlers.tokens
+    tokens: handlers.tokens,
+    checks: handlers.checks
 };
