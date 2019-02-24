0c3225fffd370a8fed0b06dc7cd93921b06a5bf8
MosesSupposes <mosamuel98@gmail.com>
2019-02-24 15:47:00 -0800

    add a test runner

---
+++ test/index.js
@@ -0,0 +1,128 @@
+/**
+ * Test Runner
+ */
+
+// Dependencies
+const assert = require('assert');
+const { outputs: clFormats } = require('../lib/cli');
+
+// Application logic for the test runner
+const _app = {
+    // Run all the tests, collecting the errors and successors
+    runTests() {
+        const limit = this.countTests();
+        const errors = [];
+        var successes = 0;
+        var counter = 0;
+
+        for (let testType in this.tests) {
+            if (this.tests.hasOwnProperty(testType)) {
+                let subTests = this.tests[testType]
+                for (let testName in subTests) {
+                    if (subTests.hasOwnProperty(testName)) {
+                        (() => {
+                            var tmpTestName = testName;
+                            var testValue = subTests[testName]
+                            // Call the test
+                            try {
+                                testValue(function done() {
+                                    // If it calls back without throwing, then it succeeded, so log it in green
+                                    console.log('\x1b[32m%s\x1b[0m', tmpTestName);
+                                    counter++;
+                                    successes++;
+                                    if (counter == limit) {
+                                        this.produceTestReport(limit, successes, errors);
+                                    }
+                                }.bind(this));
+                            } catch(e) {
+                                // If it throws, then it failed, so capture the error thrown and log it in red
+                                errors.push({
+                                    name: testName,
+                                    value: e
+                                });
+                                console.log('\x1b[31m%s\x1b[0m', tmpTestName);
+                                counter++;
+                                if (counter == limit) {
+                                    this.produceTestReport(limit, successes, errors);
+                                }
+                            }
+                        })();
+                    }
+                }
+            }
+        }
+    },
+
+    // Count all the tests
+    countTests() {
+        var counter = 0;
+        for (let testType in this.tests) {
+            if (this.tests.hasOwnProperty(testType)) {
+                let subTests = this.tests[testType];
+                for (let testName in subTests) {
+                    if (subTests.hasOwnProperty(testName)) {
+                        counter++;
+                    }
+                }
+            }
+        }
+        return counter;
+    },
+
+    // Produce a test outcome report
+    produceTestReport(limit, successes, errors) {
+        clFormats.heading('BEGIN TEST REPORT');
+        clFormats.table(
+            { 
+                'Total Tests': limit, 
+                'Passed': successes,
+                'Failed': errors.length
+            }
+        );
+
+        // If there are errors, print them in detail
+        if (errors.length > 0) {
+            clFormats.heading('BEGIN ERROR DETAILS');
+            const errorTable = errors.reduce((errTable, err) => {
+                errTable[err.name] = err.value;
+                return errTable;
+            }, {});
+            clFormats.table(errorTable);
+            clFormats.heading('END ERROR DETAILS');
+        }
+
+        clFormats.heading('END TEST REPORT');
+    }
+};
+
+// Container for the tests
+_app.tests = {
+    unit: {}
+};
+
+//#region example unit tests
+const sampleUnitTests = { getANumber() { return 1 } };
+// Assert that the getANumber function is returning a number
+_app.tests.unit['sampleUnitTests.getANumber should return a number'] = done => {
+    var val = sampleUnitTests.getANumber();
+    assert.equal(typeof(val), 'number');
+    done();
+};
+
+// Assert that the getANumber function is returning a 1
+_app.tests.unit['sampleUnitTests.getANumber should return 1'] = done => {
+    var val = sampleUnitTests.getANumber();
+    assert.equal(val, 1);
+    done();
+};
+
+// Assert that the getANumber function is returning a 2
+_app.tests.unit['sampleUnitTests.getANumber should return 2'] = done => {
+    var val = sampleUnitTests.getANumber();
+    assert.equal(val, 2);
+    done();
+};
+//#endregion
+
+// Run the tests
+_app.runTests();
