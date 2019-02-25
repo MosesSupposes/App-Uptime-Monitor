3ab8cd713001c44a9de51abf23b81e47eeba6e0c
MosesSupposes <mosamuel98@gmail.com>
2019-02-25 15:18:22 -0800

    make the app's main init method accept a callback as a param

--- index.js
+++ index.js
@@ -8,17 +8,24 @@ const workers = require('./lib/workers');
 const cli = require('./lib/cli');
 
 const app = {
-    init() {
+    init(cb) {
         // Start the server
         server.init();
         // Start the workers
         workers.init();
         // Start the CLI, but make sure it starts last
-        setImmediate(() => cli.init());
+        setImmediate(() => {
+            cli.init();
+            // Finally, callback
+            cb && cb();
+        });
     }
 };
 
-app.init();
+// The app will init on its own only under the condition that it is being required directly (i.e. it's being called with the command 'node index.js' from the command line).
+if (require.main === module) {
+    app.init();
+}
 
 module.exports = app;
 
